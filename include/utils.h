5597d174120e2b0b138901b823822e445893b833
pengwushao <2232967457@qq.com>
2021-11-10 21:22:25 +0800

    添加当前路径，资源路径，字符串替换工具函数

--- include/utils.h
+++ include/utils.h
@@ -3,9 +3,30 @@
 
 #include "game_assistant_global.h"
 
+#include <QApplication>
+#include <QString>
+
 NAMESPACE_GAME_ASSISTANT_BEGIN
 namespace utils {
+  static QString get_current_dir() {
+    static QString current_dir = QApplication::applicationDirPath();
+    return current_dir;
+  }
 
+  static QString get_resource_dir() {
+    static QString resource_dir = get_current_dir() + "../resource/";
+    return resource_dir;
+  }
+
+  static QString string_replace_all(const QString& src, const QString& old_value, const QString& new_value) {
+    QString str = src;
+    for (int pos = 0; pos < str.length(); pos += new_value.length()) {
+      if ((pos = str.indexOf(old_value, pos) != -1))
+        str.replace(pos, old_value.length(), new_value);
+      else
+        break;
+    }
+  }
 }
 NAMESPACE_GAME_ASSISTANT_END
 #endif
