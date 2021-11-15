a6ee0649a9453703f54b331f2b07557b57658fe0
pengwushao <2232967457@qq.com>
2021-11-15 23:42:03 +0800

    完善CallCommand

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,4 +1,7 @@
 #include "controller.h"
+#include <QProcess>
+#include <QMutex>
+#include <QDebug>
 
 USING_NAMESPACE_GAME_ASSISTANT
 
@@ -9,11 +12,23 @@ Controller::Controller()
 
 QPair<bool, QVector<unsigned char>> Controller::CallCommand(const QString& cmd)
 {
+  static QMutex mutex;
+  QMutexLocker locker(&mutex);
+
+  QProcess process;
+  process.start(cmd);
+  process.waitForFinished(0);
+
+  //处理返回的数据
+  qDebug() << QString::fromLocal8Bit(process.readAllStandardOutput());
   return QPair<bool, QVector<unsigned char>>();
 }
 
 bool Controller::Screencap()
 {
+  //处理截图的命令
+
+  //调用 CallCommand
   return true;
 }
 
